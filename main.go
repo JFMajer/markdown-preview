166c957354a47b9fb6c40c41303bdd667bbc316e
JFMajer <jakubmajer@live.com>
2024-01-01 16:44:17 +0100

    converted file is opened in default web browser

--- main.go
+++ main.go
@@ -4,15 +4,19 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/gomarkdown/markdown"
 	"github.com/microcosm-cc/bluemonday"
 )
 
 const (
+	// HTMLHeader and HTMLFooter are used to wrap the converted Markdown content
 	HTMLHeader = `<!DOCTYPE html>
 	<html>
 	<head>
@@ -26,7 +30,7 @@ const (
 )
 
 func main() {
-
+	// Parse command line arguments
 	filename := flag.String("file", "", "Markdown file to preview")
 	flag.Parse()
 	if *filename == "" {
@@ -34,28 +38,41 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Call the run function and handle any errors
 	err := run(*filename)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-
 }
 
 func run(filename string) error {
+	// Create a temporary directory to store the output HTML files
+	err := os.Mkdir("./tmp", 0755)
+	// If the directory exists ignore the error
+	if err != nil && !os.IsExist(err) {
+		log.Fatal(err)
+	}
+	// Read the input file
 	input, err := os.ReadFile(filename)
 	if err != nil {
 		return err
 	}
 
+	// Convert the Markdown to HTML
 	htmlData := parseMarkdown(input)
+	// Combine the HTML header, body, and footer
 	htmlComplete := generateHTML(HTMLHeader, htmlData, HTMLFooter)
+	// Create the output filename, adding a timestamp to make it unique
 	baseFilename := strings.TrimSuffix(filepath.Base(filename), ".md")
-	outName := fmt.Sprintf("%s.html", baseFilename)
+	timestamp := time.Now().Format("20060102-150405")
+	outName := fmt.Sprintf("%s-%s.html", baseFilename, timestamp)
+	// Save the output HTML to a file in the temporary directory
 	return saveHTML(outName, htmlComplete)
 }
 
 func parseMarkdown(input []byte) []byte {
+	// Convert the Markdown to HTML, then sanitize it to remove any potentially unsafe content
 	unsafeHTML := markdown.ToHTML(input, nil, nil)
 	html := bluemonday.UGCPolicy().SanitizeBytes(unsafeHTML)
 
@@ -63,18 +80,33 @@ func parseMarkdown(input []byte) []byte {
 }
 
 func saveHTML(filename string, data []byte) error {
-	err := os.WriteFile(filename, data, 0644)
+	// Write the HTML data to a file in the temporary directory
+	err := os.WriteFile(filepath.Join("./tmp", filename), data, 0644)
 	if err != nil {
 		return err
 	}
+	// Open the HTML file in the default web browser
+	fullPath := filepath.Join("./tmp", filename)
+	openWebBrowser(fullPath)
 	return nil
 }
 
 func generateHTML(header string, body []byte, footer string) []byte {
+	// Combine the header, body, and footer into a single byte slice
 	var b bytes.Buffer
 	b.WriteString(header)
 	b.Write(body)
 	b.WriteString(footer)
 
 	return b.Bytes()
+}
+
+func openWebBrowser(filename string) error {
+	// Open the output HTML file in the default web browser
+	cmd := exec.Command("xdg-open", filename)
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("failed to open web browser: %w", err)
+	}
+	return nil
+
 }
